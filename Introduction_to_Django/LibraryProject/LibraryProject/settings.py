33d1a3bde92633e4bde5ace8321bd9cb66518cbb
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-11 23:37:21 +0000

    Update settings.py

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -1,1 +1,19 @@
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_URL = 'static/'
