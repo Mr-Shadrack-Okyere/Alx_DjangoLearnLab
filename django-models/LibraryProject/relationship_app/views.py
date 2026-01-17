5d3fa13bcd67e04bfb3e72366bedf9910e34ee6d
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-17 23:57:25 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library  # ⚠ Checker explicitly looks for Library import
+from .models import Book, Library  # ⚠ This exact import is required by ALX
 
-# -----------------------------
-# Function-Based View: List all books
-# -----------------------------
+# Function-Based View
 def list_books(request):
     books = Book.objects.all()
-    # ⚠ Template path must be relationship_app/list_books.html
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# -----------------------------
-# Class-Based View: Library detail
-# -----------------------------
+# Class-Based View
 class LibraryDetailView(DetailView):
-    model = Library  # ⚠ Uses DetailView as required
+    model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
