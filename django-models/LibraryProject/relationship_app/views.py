a2eba54bfed03dc321e34e74d2e542d04449ac5b
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-17 23:45:48 +0000

    Create views.py

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
+
+# -----------------------------
+# Function-Based View
+# -----------------------------
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
