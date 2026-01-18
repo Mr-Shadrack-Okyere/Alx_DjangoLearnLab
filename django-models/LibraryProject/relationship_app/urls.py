1df2b2c168b62d27f730d5ee6d74556f09fd262a
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-18 00:14:41 +0000

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,14 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),  # function-based view
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # class-based view
-]
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),  # include app URLs
+    # Authentication URLs
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
