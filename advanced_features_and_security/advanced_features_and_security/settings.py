9f5847eaf8e66168080d2a172ba3130541b491ec
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-24 23:48:58 +0000

    Create settings.py

---
+++ advanced_features_and_security/advanced_features_and_security/settings.py
@@ -0,0 +1,18 @@
+# Custom User
+AUTH_USER_MODEL = 'users.CustomUser'
+
+# Security Settings
+DEBUG = False
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+
+# CSP example (requires django-csp installed)
+CSP_DEFAULT_SRC = ("'self'",)
+CSP_SCRIPT_SRC = ("'self'",)
