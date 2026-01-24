33af2988fa574f9dfdcd68277b0a61b26ffe3583
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-24 23:59:58 +0000

    Update models.py

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.conf import settings
 
-class Recipe(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
