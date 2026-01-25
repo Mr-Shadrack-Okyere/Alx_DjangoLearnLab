70b9a6afd7ff63bb42758be3a6eeb9c3beca8336
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-25 23:43:28 +0000

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -1,1 +1,8 @@
+# settings.py
 
+# Point to the custom user model
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
+# Media settings (if using profile_photo)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
