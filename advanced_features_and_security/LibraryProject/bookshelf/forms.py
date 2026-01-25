beb38fc1198900fedd7098b3a7e68b0b889934d0
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-25 23:49:28 +0000

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -10,3 +10,14 @@ class ExampleForm(forms.ModelForm):
             'published_date': forms.DateInput(attrs={'type': 'date'}),
         }
 
+from .forms import ExampleForm
+
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
