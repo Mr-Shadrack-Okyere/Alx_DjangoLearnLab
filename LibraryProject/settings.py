aa851b0ea657e34ac003f2e0aa2972c0b37a9d9f
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-25 00:26:21 +0000

    Create settings.py

---
+++ LibraryProject/settings.py
@@ -0,0 +1,14 @@
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "users",
+    "bookshelf",
+]
+
+AUTH_USER_MODEL = "users.CustomUser"
+
+DEBUG = True
