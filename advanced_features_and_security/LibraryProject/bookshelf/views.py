6c83e828a9cc035ce3263f9050cc741859f716ee
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-25 23:51:08 +0000

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -31,3 +31,19 @@ def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book.delete()
     return redirect('book_list')
+
+from django.shortcuts import render, redirect
+from .models import Book
+from .forms import ExampleForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
