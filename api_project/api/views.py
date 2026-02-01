60fba8b15e544599914538c3f9de4f8f135f3e2e
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-01 00:26:28 +0000

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,3 +6,18 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+# api/views.py
+from rest_framework import generics, viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+# Existing ListAPIView
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# New ViewSet for CRUD
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
