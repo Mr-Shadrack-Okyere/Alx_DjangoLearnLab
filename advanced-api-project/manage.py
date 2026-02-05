93843599ec9e31f9ce00031a45c17f603b0e5b5e
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-05 22:38:17 +0000

    Update manage.py

--- advanced-api-project/manage.py
+++ advanced-api-project/manage.py
@@ -1,11 +1,4 @@
-from api.models import Author, Book
-from api.serializers import AuthorSerializer, BookSerializer
+import django
+from django.conf import settings
 
-a = Author.objects.create(name="J.K. Rowling")
-b = Book.objects.create(title="Harry Potter", publication_year=1997, author=a)
-
-# Serialize Book
-print(BookSerializer(b).data)
-
-# Serialize Author with nested books
-print(AuthorSerializer(a).data)
+print(settings.INSTALLED_APPS)
