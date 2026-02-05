a3461db0e1b1e7679619680c722515590fcb7104
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-05 21:44:38 +0000

    Update manage.py

--- advanced-api-project/manage.py
+++ advanced-api-project/manage.py
@@ -8,3 +8,18 @@ b = Book.objects.create(title="Harry Potter", publication_year=1997, author=a)
 
 # Check the books for the author
 print(a.books.all())
+
+from api.models import Author, Book
+from api.serializers import AuthorSerializer, BookSerializer
+
+# Create sample author and book
+a = Author.objects.create(name="J.K. Rowling")
+b = Book.objects.create(title="Harry Potter", publication_year=1997, author=a)
+
+# Serialize Book
+book_serializer = BookSerializer(b)
+print(book_serializer.data)
+
+# Serialize Author (nested books)
+author_serializer = AuthorSerializer(a)
+print(author_serializer.data)
