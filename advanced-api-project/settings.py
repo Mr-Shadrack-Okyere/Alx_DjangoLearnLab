2aa84fbdcbda9b226d2f186622f911a06a40d005
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-05 21:51:09 +0000

    Update settings.py

--- advanced-api-project/settings.py
+++ advanced-api-project/settings.py
@@ -6,6 +6,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework',  # ✅ DRF installed
+    'rest_framework',  # ✅ Must be present
     'api',             # ✅ Your app
 ]
