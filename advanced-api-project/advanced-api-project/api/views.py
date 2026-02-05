097af8aac0e90dca733a6599473376a83079dc83
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-05 21:34:17 +0000

    Update views.py

--- advanced-api-project/advanced-api-project/api/views.py
+++ advanced-api-project/advanced-api-project/api/views.py
@@ -1,6 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Author
+from .serializers import AuthorSerializer
 
-class TestView(APIView):
+class AuthorListView(APIView):
     def get(self, request):
-        return Response({"message": "DRF is working!"})
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
