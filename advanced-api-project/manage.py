86fbd6ee3a52f1d42629f39f71ff9b4df35a83c1
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-05 21:37:51 +0000

    Update manage.py

--- advanced-api-project/manage.py
+++ advanced-api-project/manage.py
@@ -1,1 +1,10 @@
+from api.models import Author, Book
 
+# Create an author
+a = Author.objects.create(name="J.K. Rowling")
+
+# Create a book
+b = Book.objects.create(title="Harry Potter", publication_year=1997, author=a)
+
+# Check author books
+print(a.books.all())  # Should include the Harry Potter book
