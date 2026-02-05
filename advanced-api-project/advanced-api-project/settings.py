0f2832b5b99ebeff4748a43712a73b185b4b46a1
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-05 22:29:48 +0000

    Update settings.py

--- advanced-api-project/advanced-api-project/settings.py
+++ advanced-api-project/advanced-api-project/settings.py
@@ -5,7 +5,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'rest_framework',  # ✅ DRF added
-    'api',             # ✅ Your app
+    'rest_framework',   # Add DRF
+    'api',              # Add your API app
 ]
+
+# Use default SQLite DB (no changes needed unless you want another DB)
