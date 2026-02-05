a87acfb8412170950f363c78545a7b1ab71831a9
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-05 22:35:13 +0000

    Update settings.py

--- advanced-api-project/advanced-api-project/settings.py
+++ advanced-api-project/advanced-api-project/settings.py
@@ -5,17 +5,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Add your apps here
-]
-
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework',  # <-- Add this line
     'api',             # <-- Your API app
 ]
