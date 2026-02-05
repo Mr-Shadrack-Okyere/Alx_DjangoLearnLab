710e36f69a1029b947bdb2a44bb27fc00d609154
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-05 21:52:10 +0000

    Update models.py

--- advanced-api-project/advanced-api-project/api/models.py
+++ advanced-api-project/advanced-api-project/api/models.py
@@ -37,3 +37,12 @@ class Book(models.Model):
         """
         if self.publication_year > date.today().year:
             raise ValidationError("Publication year cannot be in the future")
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
