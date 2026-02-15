cdace3e8f5101df3481e3ac4f51eb8af0076e994
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-15 00:41:03 +0000

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -10,3 +10,28 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+from django import forms
+from .models import Post, Comment
+from taggit.forms import TagWidget
+
+
+class PostForm(forms.ModelForm):
+    tags = forms.CharField(required=False, widget=TagWidget())
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': TagWidget(),
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }
