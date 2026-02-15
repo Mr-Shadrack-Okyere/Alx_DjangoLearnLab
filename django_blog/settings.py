005b116b1a2591dc40ec4c0e15165e71402950dd
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-15 00:09:26 +0000

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -10,9 +10,15 @@ INSTALLED_APPS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog_db',       # your database name
+        'USER': 'your_db_user',         # your PostgreSQL username
+        'PASSWORD': 'your_db_password', # your PostgreSQL password
+        'HOST': 'localhost',            # database host
+        'PORT': '5432',                 # default PostgreSQL port
     }
+}
+
 }
 
 
