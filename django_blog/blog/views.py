b330ebfb88ca27d86a14d9641c7e5f30117cf986
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-15 00:42:49 +0000

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -278,3 +278,21 @@ def search_posts(request):
         'query': query,
         'results': results
     })
+from django.views.generic import ListView
+from taggit.models import Tag
+from .models import Post
+
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/post_list_by_tag.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get('tag_slug')
+        return Post.objects.filter(tags__slug=tag_slug)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.kwargs.get('tag_slug')
+        return context
