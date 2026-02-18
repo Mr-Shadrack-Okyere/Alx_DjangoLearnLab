d170b343446d141e6b003dce76c21e7bc75d2040
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-18 01:42:06 +0000

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -35,3 +35,21 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializer
+
+class FeedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        # Get posts from users that the current user follows
+        followed_users = user.following.all()
+        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
