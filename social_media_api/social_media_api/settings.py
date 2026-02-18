e80538130a73a31266afdcab55fe5b8759bd408a
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-18 01:38:01 +0000

    Update settings.py\

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -1,16 +1,13 @@
 INSTALLED_APPS = [
-    ...
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
+    'posts',  # ✅ Add this line
 ]
-
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-}
