0051391575b7dee4cfd76e130e3a411503d39024
Shadrack Okyere <masterokyere100@gmail.com>
2026-02-18 01:26:53 +0000

    Update settings.py

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -4,3 +4,13 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'accounts',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
